81ffaf22b4610a1c8d83e8105f62817a7c278349
Jan Schaffranek <schaffranek.jan@web.de>
2025-10-14 08:22:25 +0200

    Remove combat buffer from Shared.cpp
    Removed combat buffer and related index variables.

--- src/Shared.cpp
+++ src/Shared.cpp
@@ -15,10 +15,6 @@ RTAPI::RealTimeData *RTAPIData = nullptr;
 std::string AccountName;
 ArcDPS::Exports ArcExports = {};
 
-std::array<EvCombatDataPersistent, 10> combat_buffer = {};
-size_t combat_buffer_index = 0;
-size_t prev_combat_buffer_index = 0;
-
 std::filesystem::path logpath;
 
 RotationRun rotation_run{};
