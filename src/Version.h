209f8fd212d50bb701c1e437f64a38d124c464fe
Jan Schaffranek <schaffranek.jan@web.de>
2025-10-20 13:18:39 +0200

    added macros

--- src/Version.h
+++ src/Version.h
@@ -6,4 +6,11 @@
 #define BUILD 0
 #define REVISION 0
 
+// Macro to convert a number to string
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+// Macro to build version string
+#define VERSION_STRING TOSTRING(MAJOR) "." TOSTRING(MINOR) "." TOSTRING(BUILD)
+
 #endif // VERSION_H
