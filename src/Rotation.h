6f3d90bebaec3c78f2cf42763e9ea9360e34b307
Jan Schaffranek <schaffranek.jan@web.de>
2025-11-22 19:04:09 +0100

    refactor

--- src/Rotation.h
+++ src/Rotation.h
@@ -10,5 +10,4 @@
 void SkillDetectionLogic(uint32_t &num_skills_wo_match,
                          std::chrono::steady_clock::time_point &time_since_last_match,
                          RotationLogType &rotation_run,
-                         const EvCombatDataPersistent &skill_ev,
-                         EvCombatDataPersistent &last_skill);
+                         const EvCombatDataPersistent &skill_ev);
