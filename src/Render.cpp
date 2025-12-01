2d0f8f22066c8caa2992e25e0c7e9bde3f0d6e8e
Jan Schaffranek <schaffranek.jan@web.de>
2025-12-01 17:09:51 +0100

    added link

--- src/Render.cpp
+++ src/Render.cpp
@@ -634,6 +634,8 @@ void RenderType::render_options_window(bool &is_not_ui_adjust_active)
         if (Globals::BenchDataDownloadState == DownloadState::FAILED)
         {
             ImGui::Text("Failed Downloading/Extracting Bench Data. Please reach out to me.");
+            ImGui::Text("You can download it manually from GitHub see: ");
+            ImGui::Text("https://github.com/franneck94/GW2_RotaHelper?tab=readme-ov-file#benchmark-file-downloadextraction-fails");
             ImGui::End();
             return;
         }
